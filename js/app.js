b8ad5f6814a564428c0371ca99966099408ce044
AALKANEE <142936918+AALKANEE@users.noreply.github.com>
2024-05-01 20:51:54 +0330

    html class

--- js/app.js
+++ js/app.js
@@ -5,12 +5,23 @@ class Budget{
     }
 }
 
+// every thing Related to the Html
+class HTML{
+    //insert user budget to the html
+    insertBudget(amount){
+        console.log(budgetTotal)
+        console.log(budgetLeft)
+    }
+
+}
 
 
 // variable
 let userBudget;
 let budget;
-
+let budgetTotal=document.querySelector('span#total')
+let budgetLeft=document.querySelector('span#left')
+const html=new HTML()
 
 
 
@@ -29,6 +40,7 @@ function eventListeners(){
             // instanciate Budget class
             budget =new Budget(userBudget)
             console.log(budget)
+            html.insertBudget(budget.budget)
         }
     })
 }
